0a4fe26490803026c477e69a1c12361a3d4b0b8e
LevCed <ced91levesque@gmail.com>
2018-01-09 17:21:07 -0500

    Importation of the AssassinGO project from my school github to my personnal github.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // reference to the GameBoard
+    Board m_board;
+
+    // reference to PlayerManager
+    PlayerManager m_player;
+
+    // has the user pressed start?
+    bool m_hasLevelStarted = false;
+    public bool HasLevelStarted { get { return m_hasLevelStarted; } set { m_hasLevelStarted = value; } }
+
+    // have we begun gamePlay?
+    bool m_isGamePlaying = false;
+    public bool IsGamePlaying { get { return m_isGamePlaying; } set { m_isGamePlaying = value; } }
+
+    // have we met the game over condition?
+    bool m_isGameOver = false;
+    public bool IsGameOver { get { return m_isGameOver; } set { m_isGameOver = value; } }
+
+    // have the end level graphics finished playing?
+    bool m_hasLevelFinished = false;
+    public bool HasLevelFinished { get { return m_hasLevelFinished; } set { m_hasLevelFinished = value; } }
+
+    // delay in between game stages
+    public float delay = 1f;
+
+    // events invoked for StartLevel/PlayLevel/EndLevel coroutines
+    public UnityEvent startLevelEvent;
+    public UnityEvent playLevelEvent;
+    public UnityEvent endLevelEvent;
+
+    void Awake()
+    {
+        // populate Board and PlayerManager components
+        m_board = Object.FindObjectOfType<Board>().GetComponent<Board>();
+        m_player = Object.FindObjectOfType<PlayerManager>().GetComponent<PlayerManager>();
+    }
+
+    void Start()
+    {
+        // start the main game loop if the PlayerManager and Board are present
+        if (m_player != null && m_board != null)
+        {
+            StartCoroutine("RunGameLoop");
+        }
+        else
+        {
+            Debug.LogWarning("GAMEMANAGER Error: no player or board found!");
+        }
+    }
+
+    // run the main game loop, separated into different stages/coroutines
+    IEnumerator RunGameLoop()
+    {
+        yield return StartCoroutine("StartLevelRoutine");
+        yield return StartCoroutine("PlayLevelRoutine");
+        yield return StartCoroutine("EndLevelRoutine");
+    }
+
+    // the initial stage after the level is loaded
+    IEnumerator StartLevelRoutine()
+    {
+        Debug.Log("START LEVEL");
+        m_player.playerInput.InputEnabled = false;
+        while (!m_hasLevelStarted)
+        {
+            //show start screen
+            // user presses button to start
+            // HasLevelStarted = true
+            yield return null;
+        }
+
+        // trigger events when we press the StartButton
+        if (startLevelEvent != null)
+        {
+            startLevelEvent.Invoke();
+        }
+    }
+
+    // gameplay stage
+    IEnumerator PlayLevelRoutine()
+    {
+        Debug.Log("PLAY LEVEL");
+        m_isGamePlaying = true;
+        yield return new WaitForSeconds(delay);
+        m_player.playerInput.InputEnabled = true;
+
+        // trigger any events as we start playing the level
+        if (playLevelEvent != null)
+        {
+            playLevelEvent.Invoke();
+        }
+
+        while (!m_isGameOver)
+        {
+        	// pause one frame
+			yield return null;
+
+            // check for level win condition
+             m_isGameOver = IsWinner();
+
+			// check for the lose condition
+        }
+        // Debug.Log("WIN! ==========================");
+    }
+
+    // end stage after gameplay is complete
+    IEnumerator EndLevelRoutine()
+    {
+        Debug.Log("END LEVEL");
+        m_player.playerInput.InputEnabled = false;
+
+        // run events when we end the level
+        if (endLevelEvent != null)
+        {
+            endLevelEvent.Invoke();
+        }
+
+        // show end screen
+        while (!m_hasLevelFinished)
+        {
+            // user presses button to continue
+
+            // HasLevelFinished = true
+            yield return null;
+        }
+
+        // reload the current scene
+        RestartLevel();
+    }
+
+    // restart the current level
+    void RestartLevel()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    // attach to StartButton, triggers PlayLevelRoutine
+    public void PlayLevel()
+    {
+        m_hasLevelStarted = true;
+    }
+
+    // has the player reached the goal node?
+    bool IsWinner()
+    {
+        if (m_board.PlayerNode != null)
+        {
+            return (m_board.PlayerNode == m_board.GoalNode);
+        }
+        return false;
+    }
+
+}
